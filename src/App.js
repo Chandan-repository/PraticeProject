549e158a27c53767a6627301e20521ec97021d08
Chandan-repository <67368366+Chandan-repository@users.noreply.github.com>
2023-03-10 09:31:05 +0530

    ManggingList of user state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
-import AddUser  from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
-import "./style.css";
+import React, { useState } from 'react';
 
-export default function App() {
-    return (
-      <div>
-        <AddUser />
-        <UsersList users={[]} />
-      </div>
-    );
-  
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
 }
+
+export default App;
