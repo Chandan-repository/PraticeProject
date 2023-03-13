1d2b40849c695e5c36ff95f1bcfbac8cfe4e32fd
Chandan-repository <67368366+Chandan-repository@users.noreply.github.com>
2023-03-13 11:12:32 +0530

    managing error state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,6 +8,7 @@ import classes from './AddUser.module.css';
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
@@ -32,7 +33,13 @@ const AddUser = (props) => {
 
   return (
     <div>
-    <ErrorModal title="An error occured!" message="Something went wrong!" />
+     {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
